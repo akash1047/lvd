996b6be94057e00d01574be0e55f06372b0b62e5
Bhargav <bhargav200004@gmail.com>
2025-10-27 21:18:23 +0530

    feat: vulkan instance initiation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include "vulkan/vulkan_core.h"
+#include <GLFW/glfw3.h>
 #include <exception>
+#include <vulkan/vulkan.hpp>
 
 import glfw;
 import logger;
@@ -11,6 +14,50 @@ int main() {
 
     glfw::glfw glfw{error_callback};
 
+    VkInstance instance;
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
+    }
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                           extensions.data());
+
+    logger::info("available extensions:");
+
+    for (const auto &extension : extensions) {
+      logger::info("{}", extension.extensionName);
+    }
+
     glfw.no_api();
     glfw.resizable(false);
 
@@ -19,6 +66,8 @@ int main() {
     while (!window.should_close()) {
       glfw.poll_events();
     }
+
+    vkDestroyInstance(instance, nullptr);
 
   } catch (const std::exception &e) {
     logger::critical("Exception: {}", e.what());
